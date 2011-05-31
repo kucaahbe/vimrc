5fbfc5b2353b5cee5c3ba9f0be25bcd1a1db12eb
kucaahbe <kucaahbe@ukr.net>
2011-05-31 22:54:19 +0300

    added gmivrc where removed standard menu

---
+++ gvimrc
@@ -0,0 +1,2 @@
+set guioptions+=M "the default menus are not loaded
+set guioptions-=m "the menubar is not displayed
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 DIR=`dirname $(readlink -f $0)`
 ln -sfv $DIR/vim ~/.vim
 ln -sfv $DIR/vimrc ~/.vimrc
+ln -sfv $DIR/gvimrc ~/.gvimrc
