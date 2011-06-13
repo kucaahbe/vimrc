6245f01b3da0f729377ebd6a76bbdf7972815431
kucaahbe <kucaahbe@ukr.net>
2011-06-13 17:11:34 +0300

    comments

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 DIR=`dirname $(readlink -f $0)`
+# vim config directory
 ln -sfv -T $DIR/vim ~/.vim
+# installing pathogen
 ln -sfv $DIR/pathogen/autoload/pathogen.vim ~/.vim/autoload/
+# vimrc and gvimrc:
 ln -sfv $DIR/vimrc ~/.vimrc
 ln -sfv $DIR/gvimrc ~/.gvimrc
