be7b7c8eed2d6ba1aacfdfa97dc3a86796175287
Dmitry Mjakotny <dikuca@gmail.com>
2012-01-31 18:49:36 +0200

    fixed pathogen issue

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 DIR=`dirname $(readlink -f $0)`
 # vim config directory
 ln -sfv -T $DIR/vim ~/.vim
-# installing pathogen
-ln -sfv $DIR/pathogen/autoload/pathogen.vim ~/.vim/autoload/
 # vimrc and gvimrc:
 ln -sfv $DIR/vimrc ~/.vimrc
 ln -sfv $DIR/gvimrc ~/.gvimrc
